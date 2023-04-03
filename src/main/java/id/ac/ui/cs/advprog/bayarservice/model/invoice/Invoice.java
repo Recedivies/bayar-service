5b7e8967235f4b32b293dd6c06d5d94eca89f323
recedivies <ahmadhiprananta@gmail.com>
2023-04-03 13:29:10 +0700

    Init Invoice & Bill model

---
+++ src/main/java/id/ac/ui/cs/advprog/bayarservice/model/invoice/Invoice.java
@@ -0,0 +1,44 @@
+package id.ac.ui.cs.advprog.bayarservice.model.invoice;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import id.ac.ui.cs.advprog.bayarservice.model.bill.Bill;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.sql.Date;
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "invoices")
+public class Invoice {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    @Column(nullable = false, columnDefinition = "VARCHAR(5)")
+    @Enumerated(EnumType.STRING)
+    private PaymentMethod paymentMethod;
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false, columnDefinition = "TIMESTAMP WITH TIME ZONE default CURRENT_TIMESTAMP")
+    private Date createdAt;
+    @Column(nullable = false)
+    private Integer totalAmount;
+    @Column(nullable = false)
+    private Integer adminFee;
+    @Column(nullable = false, columnDefinition = "int default 0")
+    private Integer discount;
+    @JsonManagedReference
+    @OneToMany(mappedBy = "invoice", cascade = CascadeType.ALL)
+    private List<Bill> bills;
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(nullable = false)
+    private UUID sessionId;
+}
