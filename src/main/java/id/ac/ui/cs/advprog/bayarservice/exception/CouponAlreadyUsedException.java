8a31873cdd50275bf499391f1cf0a7976f1a2f2e
recedivies <ahmadhiprananta@gmail.com>
2023-04-21 21:50:56 +0700

    fix: message error coupon already used

--- src/main/java/id/ac/ui/cs/advprog/bayarservice/exception/CouponAlreadyUsedException.java
+++ src/main/java/id/ac/ui/cs/advprog/bayarservice/exception/CouponAlreadyUsedException.java
@@ -2,6 +2,6 @@ package id.ac.ui.cs.advprog.bayarservice.exception;
 
 public class CouponAlreadyUsedException extends RuntimeException {
     public CouponAlreadyUsedException(String couponName) {
-        super("Coupon with name " + couponName + " does not exist");
+        super("Coupon with name " + couponName + " already used");
     }
 }
