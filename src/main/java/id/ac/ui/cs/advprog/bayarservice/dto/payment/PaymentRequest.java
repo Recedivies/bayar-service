a8f86becbc015d937a138532656b2f9d86607425
recedivies <ahmadhiprananta@gmail.com>
2023-05-07 14:27:04 +0700

    fix: smell code

--- src/main/java/id/ac/ui/cs/advprog/bayarservice/dto/payment/PaymentRequest.java
+++ src/main/java/id/ac/ui/cs/advprog/bayarservice/dto/payment/PaymentRequest.java
@@ -1,6 +1,5 @@
 package id.ac.ui.cs.advprog.bayarservice.dto.payment;
 
-import jakarta.persistence.Column;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
